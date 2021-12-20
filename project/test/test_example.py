201c0f93c9451e41da0d43fd6cac4c7e57758bac
JuniorTenorio16 <juniortenorio1016@gmail.com>
2021-12-20 03:08:08 -0300

    example test

---
+++ project/test/test_example.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.fixture
+def function_fixture():
+   print('Fixture for each test')
+   return 1
+
+
+@pytest.fixture(scope='module')
+def module_fixture():
+   print('Fixture for module')
+   return 2
